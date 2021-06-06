6fde11826990f0b420edb741d20853e29d0440dd
alvaro lavoizier <alvarolavoizier@gmail.com>
2021-06-06 15:43:44 +0800

    migrasi

--- example/indonesia_example.dart
+++ example/indonesia_example.dart
@@ -1,13 +1,9 @@
-import 'package:indonesia/indonesia.dart';
+import 'package:tanggal_indonesia/tanggal_indonesia.dart';
 
 main() {
   // Currency
   rupiah(123456789); // Rp 123.456.789
-  rupiah(
-    123456789,
-    separator: ',',
-    trailing: '.00'
-  ); // Rp 123,456,789.00
+  rupiah(123456789, separator: ',', trailing: '.00'); // Rp 123,456,789.00
 
   // Date
   DateTime date = DateTime.now();
